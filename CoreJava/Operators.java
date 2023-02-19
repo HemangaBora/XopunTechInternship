d06acaf36be1e72b06ea0b21e6363cef681affdf
Hemanga Bora <hemangabora62@gmail.com>
2023-02-19 22:38:24 +0530

    operators

---
+++ CoreJava/Operators.java
@@ -0,0 +1,32 @@
+package CoreJava;
+
+//Arithmetic Operators
+
+class Main {
+    public static void main(String[] args) {
+      
+      // declare variables
+     int a = 12, b = 5;
+  
+      // addition operator
+      System.out.println("a + b = " + (a + b));
+    }
+}
+
+//Assignment Operators
+
+class Main {
+    public static void main(String[] args) {
+      
+      // create variables
+      int a = 4;
+      int var;
+  
+      // assign value using =
+      var = a;
+      System.out.println("var using =: " + var);
+    }
+}
+
+
+
