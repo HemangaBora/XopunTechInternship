a83d5ecc9c6ebfde434da77772fa06dc10682873
Hemanga Bora <hemangabora62@gmail.com>
2023-02-19 22:54:49 +0530

    assignment op

--- CoreJava/Arithmetic_Operators.java
+++ CoreJava/Arithmetic_Operators.java
@@ -15,18 +15,6 @@ class Main {
 
 //Assignment Operators
 
-class Main {
-    public static void main(String[] args) {
-      
-      // create variables
-      int a = 4;
-      int var;
-  
-      // assign value using =
-      var = a;
-      System.out.println("var using =: " + var);
-    }
-}
 
 
 
