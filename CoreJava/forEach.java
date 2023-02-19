4d0c5a3a44015c621fcc7aa63acb5031a2c48d90
Hemanga Bora <hemangabora62@gmail.com>
2023-02-19 23:47:20 +0530

    core java

---
+++ CoreJava/forEach.java
@@ -0,0 +1,16 @@
+package CoreJava;
+
+public class forEach {
+    public static void main(String[] args)
+  
+    {
+  
+        // array declaration
+  
+        int ar[] = { 10, 50, 60, 80, 90 };
+  
+        for (int element : ar)
+  
+            System.out.print(element + " ");
+    }
+}
