6bba3b675a41657c77462b29d21829ae04cc2c05
Hemanga Bora <hemangabora62@gmail.com>
2023-02-19 14:36:25 +0530

    variables

---
+++ CoreJava/Variables.java
@@ -0,0 +1,48 @@
+package CoreJava;
+// 1st program
+
+class Test
+{
+    public static void main(String args[])
+    {
+        System.out.print("hello World !!");
+    }
+}
+
+// Variables
+
+class Variables
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        System.out.println(a);
+
+        String name = "John";
+        System.out.println(name);
+
+        // Type Casting
+        //Implicit Type Casting
+
+        int i = 50;
+        
+        double d = i;
+
+        System.out.println(i);
+        System.out.println(d);
+
+        // Explicit Type Casting
+
+        double myvul = 40.22;
+
+        int myint = (int)myvul;
+
+        System.out.println(myvul);
+        System.out.println(myint);
+    }
+}
+
+
+
+
+
